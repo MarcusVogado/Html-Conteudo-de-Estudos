058f3e313fdddf5e96f61de3c8076d531d5e5181
marcus vogado <marcus_620@hotmail.com>
2022-09-09 20:09:59 -0300

    update

---
+++ arquivoJs.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+alert(" OLÁ MUNDO");
+
+document.addEventListener("DOMCONTETlOADED,"), function (event) {
+    alert('DOCUMENTO CARREGADO')
+}
+function myFunction(texto) {
+    alert("oi");
+    alert(texto);    
+}
+function redirecionarPagina() {
+
+    window.open('http://google.com', '_blank');
+}
+
+//randon sorteio de numeros 
+
+
+/*REDIRECIONAR PAGINAS EM JAVA SCRYPT*/
